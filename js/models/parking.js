ddb4b6cd4ac0461fa0d5a34f6fc32655390261ac
Antoine RICHARD <arichard@sqli.com>
2012-12-09 22:48:02 +0100

    WIP

--- js/models/parking.js
+++ js/models/parking.js
@@ -4,6 +4,15 @@ define([
 
   var Parking = Backbone.Model.extend({
 
+  	defaults: {
+  		subscriberOnly: function() {
+  			return this.Grp_statut == 2;
+  		},
+        almostFull: function() {
+            return +this.Grp_disponible <= +this.Grp_complet;
+        }
+    }
+
   });
 
   return Parking;
